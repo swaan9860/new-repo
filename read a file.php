df02cb2e544c5810b5f6f9afc88c1bc4f7c10d6c
Swaan <swaan800@gmail.com>
2023-09-14 10:23:55 +0545

    first commit

--- read a file.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$fh = fopen("testfile.txt", 'r') or die("File does not exist or you do not have the authority to access the file");
-	$text = fread($fh, 32);
-	fclose($fh);
-	echo $text;
-?>
